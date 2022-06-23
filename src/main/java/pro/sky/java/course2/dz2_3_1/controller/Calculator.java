f1c339b19d0d30391ae82ddf6b828be471c39f43
EduardGeras <103679703+EduardGeras@users.noreply.github.com>
2022-06-23 20:07:07 +0500

    Домашка 2.3 доработка

--- src/main/java/pro/sky/java/course2/dz2_3_1/controller/Calculator.java
+++ src/main/java/pro/sky/java/course2/dz2_3_1/controller/Calculator.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
+
 @RestController
 @RequestMapping("/calculator")
 public class Calculator {
@@ -21,26 +24,39 @@ public class Calculator {
     }
 
     @GetMapping("/plus")
-    public String plus(@RequestParam int num1,
-                       @RequestParam int num2) {
+    public String plus(@RequestParam (required = false) double num1,
+                       @RequestParam (required = false) double num2) {
+        if (Objects.isNull(num1) || Objects.isNull(num2)){
+            return "Введите, пожалуйста, все параметры";
+        }
         return num1 + " + " + num2 + " = " + calculatorService.plus(num1, num2);
     }
     @GetMapping("/minus")
-    public String minus(@RequestParam int num1,
-                        @RequestParam int num2) {
+    public String minus(@RequestParam (required = false) double num1,
+                        @RequestParam (required = false) double num2) {
+        if (Objects.isNull(num1) || Objects.isNull(num2)){
+            return "Введите, пожалуйста, все параметры";
+        }
         return num1 + " - " + num2 + " = " + calculatorService.minus(num1, num2);
     }
     @GetMapping("/multiply")
-    public String multiply(@RequestParam int num1,
-                           @RequestParam int num2) {
+    public String multiply(@RequestParam (required = false) double num1,
+                           @RequestParam (required = false) double num2) {
+        if (Objects.isNull(num1) || Objects.isNull(num2)){
+            return "Введите, пожалуйста, все параметры";
+        }
         return num1 + " * " + num2 + " = " + calculatorService.multiply(num1, num2);
     }
     @GetMapping("/divide")
-    public String divide(@RequestParam double num1,
-                         @RequestParam double num2) {
+    public String divide(@RequestParam (required = false) double num1,
+                         @RequestParam (required = false) double num2) {
+        if (Objects.isNull(num1) || Objects.isNull(num2)){
+            return "Введите, пожалуйста, все параметры";
+        }
         if (num2 == 0) {
             return "На нуль делить нельзя!!!";
         }
         return num1 + " / " + num2 + " = " + calculatorService.divide(num1, num2);
     }
+
 }
--- src/main/java/pro/sky/java/course2/dz2_3_1/service/CalculatorServiceIml.java
+++ src/main/java/pro/sky/java/course2/dz2_3_1/service/CalculatorServiceIml.java
@@ -5,13 +5,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class CalculatorServiceIml{
 
-    public int plus(int num1, int num2) {
+    public double plus(double num1, double num2) {
         return num1 + num2;
     }
-    public int minus(int num1, int num2) {
+    public double minus(double num1, double num2) {
         return num1 - num2;
     }
-    public int multiply(int num1, int num2) {
+    public double multiply(double num1, double num2) {
         return num1 * num2;
     }
     public double divide(double num1, double num2) {
